c3673572062442846f1a91921de5c823aca7cb6e
Your Name <j.hamwijk34@gmail.com>
2021-01-05 10:00:17 +0100

    Push First project

---
+++ First course/client/src/data.js
@@ -0,0 +1,42 @@
+export default [
+  {
+    id: 1,
+    name: 'Miss Beauty',
+    age: 29,
+    country: 'Brazil',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+  },
+  {
+    id: 2,
+    name: 'Doctor Robert',
+    age: 32,
+    country: 'United States',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+  },
+  {
+    id: 3,
+    name: 'Little Doc',
+    age: 36,
+    country: 'Spain',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 4,
+    name: 'Dr East',
+    age: 34,
+    country: 'Germany',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 5,
+    name: 'Junior Boy',
+    age: 31,
+    country: 'Netherlands',
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+  },
+];
